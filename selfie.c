350ca0f1dc8f6e5938b6726f19d78082dd538647
Johnyboy05 <johnyboy.ch@gmail.com>
2016-03-04 12:50:48 +0100

    Added Solution for A0

--- selfie.c
+++ selfie.c
@@ -6421,7 +6421,7 @@ int main(int argc, int *argv) {
 
     argc = argc - 1;
     argv = argv + 1;
-
+    print((int*)"This is Teamname Selfie");
     if (selfie(argc, (int*) argv) != 0) {
         print(selfieName);
         print((int*) ": usage: selfie { -c source | -o binary | -s assembly | -l binary } [ -m size ... | -d size ... | -y size ... ] ");
